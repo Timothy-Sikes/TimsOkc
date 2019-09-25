05f2bea99d7a678dc6d7cb12b8c3195188f49025
timothy.sikes@sonicdrivein.com <timothy.sikes@sonicdrivein.com>
2019-09-25 16:11:26 -0500

    Convert to PUG and componenitize HTML elements.

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+const pug = require('pug');
 
 app.use(express.static('./content/static/'));
 
+
 app.get('*', function (req, res) {
-  res.sendfile('./content/static/HTML/main.html');
+  const compiledFunction = pug.compileFile('./content/static/PUG/home.pug');
+
+  res.send(compiledFunction({
+    message: "A placeholder for something I might do someday maybe.",
+    name: "test",
+    url: "/images/TimsOKC/carbonara.jpg",
+    foodName: "Spaghetti Carbonara",
+    foodDate: "9/24/19",
+    foodUrl: "https://cooking.nytimes.com/recipes/12965-spaghetti-carbonara"
+  }))
+
 })
 
 app.listen((process.env.PORT || 8000));
