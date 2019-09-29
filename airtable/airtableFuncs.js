8f44a75eb50ffa429e15ca460bd4175c75d06218
timothy.sikes@sonicdrivein.com <timothy.sikes@sonicdrivein.com>
2019-09-28 20:38:06 -0500

    Grab cooking records dynamically.

---
+++ airtable/airtableFuncs.js
@@ -0,0 +1,17 @@
+function getLatestRecipe(base)
+{
+    return base('Recipe Nights').select({
+      maxRecords: 1,
+      sort: [{field: "Date", direction: "desc"}]
+    }).firstPage;
+}
+
+function getRecipeRecord(base, recordId)
+{
+    return base('Recipes').find(recordId);
+}
+
+module.exports = {
+    getLatestRecipe,
+    getRecipeRecord
+}
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var app = express();
 const pug = require('pug');
+var Airtable = require('airtable');
+var airtableFuncs = require('./airtable/airtableFuncs.js')
 
 app.use(express.static('./content/static/'));
 
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+});
+
+const base = Airtable.base('app3Q7m6yl9TLjbbP');
+const compiledFunction = pug.compileFile('./content/static/PUG/home.pug');
 
 app.get('*', function (req, res) {
-  const compiledFunction = pug.compileFile('./content/static/PUG/home.pug');
+  recipeNightsPromise = airtableFuncs.getLatestRecipe(base)
 
-  res.send(compiledFunction({
-    message: "A placeholder for something I might do someday maybe.",
-    name: "test",
-    url: "/images/TimsOKC/carbonara.jpg",
-    foodName: "Spaghetti Carbonara",
-    foodDate: "9/24/19",
-    foodUrl: "https://cooking.nytimes.com/recipes/12965-spaghetti-carbonara"
-  }))
+  recipeNightsPromise().then(function(recipeNightsRecord) {
+    recipePromise = airtableFuncs.getRecipeRecord(base, recipeNightsRecord[0].fields.Recipe[0])
 
+    recipePromise.then(function(record) {
+      res.send(compiledFunction({
+        message: "A placeholder for something I might do someday maybe.",
+        name: "test",
+        recipePic: record.fields["Attachments"][0].url || "http://orcz.com/images/7/71/BreathoftheWildDubiousFood.jpg",
+        recipeName: record.fields["Recipe Name"],
+        recipeDate: recipeNightsRecord[0].fields.Date,
+        recipeUrl: record.fields["URL"],
+        recipeSuccess: recipeNightsRecord[0].fields.Success + "!" || "🤷",
+      }))
+    })
+  })
 })
 
 app.listen((process.env.PORT || 8000));
