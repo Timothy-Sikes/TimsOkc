552dc0ff183b451238213247016d5d0231fbee34
timothy.sikes@sonicdrivein.com <timothy.sikes@sonicdrivein.com>
2019-10-01 16:03:59 -0500

    Add rotating random background.

--- server.js
+++ server.js
@@ -11,15 +11,30 @@ Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY
 });
 
-const base = Airtable.base('app3Q7m6yl9TLjbbP');
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+backgroundPaths = [
+  "/images/TimsOKC/WarpDrive.gif",
+  "/images/TimsOKC/underground_green.jpg",
+  "/images/TimsOKC/underground_blue.jpg",
+  "/images/TimsOKC/lines1.png",
+  "/images/TimsOKC/bricks2.png",
+  "/images/TimsOKC/pdx.png",
+  "/images/TimsOKC/wood-paneling.png",
+  "/images/TimsOKC/Matrix.gif"
+]
+
 const compiledFunction = pug.compileFile('./content/static/PUG/home.pug');
+const base = Airtable.base('app3Q7m6yl9TLjbbP');
 
 app.get('*', function (req, res) {
   recipeNightsPromise = airtableFuncs.getLatestRecipe(base)
 
   recipeNightsPromise().then(function(recipeNightsRecord) {
     recipePromise = airtableFuncs.getRecipeRecord(base, recipeNightsRecord[0].fields.Recipe[0])
-
+  
     recipePromise.then(function(record) {
       res.send(compiledFunction({
         message: "A placeholder for something I might do someday maybe.",
@@ -29,6 +44,8 @@ app.get('*', function (req, res) {
         recipeDate: recipeNightsRecord[0].fields.Date,
         recipeUrl: record.fields["URL"],
         recipeSuccess: recipeNightsRecord[0].fields.Success + "!" || "🤷",
+
+        backgroundImage: backgroundPaths[getRandomInt(backgroundPaths.length)]
       }))
     })
   })
